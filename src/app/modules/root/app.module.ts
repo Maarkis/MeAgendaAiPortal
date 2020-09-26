3a00bdef4ac7b4a8245172714102375002947137
Jean Markis <jeanmarkis85@gmail.com>
2020-09-26 16:57:09 -0300

    Root project

--- src/app/modules/root/app.module.ts
+++ src/app/modules/root/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+import { environment } from '../../../environments/environment';
 
 @NgModule({
   declarations: [
